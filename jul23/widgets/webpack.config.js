c820231fbb69bdc3894f504556e9a64e2bd670c8
Ed <edmundho9@gmail.com>
2018-07-23 14:16:13 -0700

    complete js30 array cardio 2 and fun with canvas. set up React widgets project

---
+++ jul23/widgets/webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module.exports = {
+  entry: './frontend/widgets.jsx',
+  output: {
+    filename: './bundle.js',
+  },
+  module: {
+    rules: [{
+      test: [/\.jsx?$/],
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      },
+    }]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
