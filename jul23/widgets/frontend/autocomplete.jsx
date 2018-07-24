a1eaea321f7bfd55a6701732fba78c771d1bcddb
Ed <edmundho9@gmail.com>
2018-07-24 14:04:23 -0700

    complete autocomplete widget

--- jul23/widgets/frontend/autocomplete.jsx
+++ jul23/widgets/frontend/autocomplete.jsx
@@ -4,13 +4,38 @@ class Autocomplete extends React.Component {
   constructor (props) {
     super(props);
 
+    this.state = {
+      inputVal: "",
+    };
 
+    this.handleChange = this.handleChange.bind(this);
+  }
+    
+  handleChange (event) {
+    this.setState({ inputVal: event.target.value });
   }
 
   render () {
+    const names = this.props.names
+      .filter((name, idx) => name.toLowerCase().includes(this.state.inputVal.toLowerCase()))
+      .map((name, idx) => {
+        return (
+          <li key={idx}>{name}</li>
+        );
+      })
+      .sort((a, b) => a - b);
+
+
     return (
       <div id="autocomplete">
-        <input type="text"/>
+        <div id="autocomplete-inner">
+          <input type="text" 
+            onChange={this.handleChange} 
+            value={this.state.inputVal}/>
+          <ul>
+            {names}
+          </ul>
+        </div>
       </div>
     );
 
