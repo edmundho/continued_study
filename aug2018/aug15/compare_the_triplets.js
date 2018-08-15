cabf8babd7a6fb41a842fe7365666d8c8df30fa3
Ed <edmundho9@gmail.com>
2018-08-15 16:21:57 -0700

    hackerrank easy warmups

---
+++ aug2018/aug15/compare_the_triplets.js
@@ -0,0 +1,14 @@
+function compareTriplets(a, b) {
+  let result = [0, 0];
+
+  for (let i = 0; i < 3; i++) {
+    if (a[i] > b[i]) {
+      result[0]++;
+    }
+    if (a[i] < b[i]) {
+      result[1]++;
+    }
+  }
+
+  return result;
+}
