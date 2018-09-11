c9af820c01fcbfb83f58bccaea68537dafc61e9b
Ed <edmundho9@gmail.com>
2018-09-10 18:43:04 -0700

    Ruby curriculum project: memory puzzle game

--- sep2018/memory_puzz_ruby/card.rb
+++ sep2018/memory_puzz_ruby/card.rb
@@ -1,11 +1,23 @@
 class Card
+  VALUES = ("A".."Z").to_a
 
-  attr_accessor :showing
   attr_reader :value
 
-  def initialize(value)
+  def self.shuffled_pairs(num_pairs)
+    values = VALUES
+
+    while num_pairs > values.length
+      values = values + values
+    end
+
+    values = values.shuffle.take(num_pairs) * 2
+    values.shuffle!
+    values.map { |val| self.new(val) }
+  end
+  
+  def initialize(value, showing = false)
     @value = value
-    @showing = false
+    @showing = showing
   end
   
   def hide
@@ -16,11 +28,15 @@ class Card
     @showing = true
   end
 
-  def to_s
-    value
+  def revealed?
+    @showing
   end
 
-  def ==(val)
-    value == val
+  def to_s
+    revealed? ? value.to_s : " "
+  end
+
+  def ==(object)
+    object.is_a?(self.class) && object.value == value
   end
 end
